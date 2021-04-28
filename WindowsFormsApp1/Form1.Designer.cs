b7cc6515465ad38b79e687a5b3aadd9fbf886118
瑋儒 黃 <well002190@gmail.com>
2021-04-28 12:59:43 +0800

    12345

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -97,6 +97,7 @@ namespace WindowsFormsApp1
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(303, 22);
             this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "12345";
             // 
             // button1
             // 
