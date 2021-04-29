c709b720ae5ef5139c34d3c87c65d6a34a54c71d
a61103mail <83195040+a61103mail@users.noreply.github.com>
2021-04-29 09:01:34 +0800

    frm2

---
+++ WindowsFormsApp1/Properties/訂貨表.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Properties
+{
+    public partial class 訂貨表 : Form
+    {
+        public 訂貨表()
+        {
+            InitializeComponent();
+        }
+    }
+}
